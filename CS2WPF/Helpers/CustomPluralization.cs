dff369a4a0d3ecdf1b81710ad3860cea0a76aa8e
chempkovsky <chempkovsky@rambler.ru>
2021-08-10 16:24:43 +0300

    php extension of backend

---
+++ CS2WPF/Helpers/CustomPluralization.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.Entity.Design.PluralizationServices;
+using System.Globalization;
+
+namespace CS2WPF.Helpers
+{
+    static class CustomPluralization
+    {
+        public static PluralizationService pluralizationservice = PluralizationService.CreateService(new CultureInfo("en-us"));
+
+        public static string Pluralize(string str)
+        {
+            if (string.IsNullOrEmpty(str)) return "";
+            if (pluralizationservice.IsPlural(str)) return str;
+            return pluralizationservice.Pluralize(str);
+        }
+        public static string Singularize(string str)
+        {
+            if (string.IsNullOrEmpty(str)) return "";
+            if (pluralizationservice.IsSingular(str)) return str;
+            return pluralizationservice.Singularize(str);
+        }
+        public static bool IsSingular(string str)
+        {
+            return pluralizationservice.IsSingular(str);
+        }
+        public static bool IsPlural(string str)
+        {
+            return pluralizationservice.IsPlural(str);
+        }
+    }
+}
