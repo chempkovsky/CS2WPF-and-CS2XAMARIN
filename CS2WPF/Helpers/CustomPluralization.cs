3f9708b67e94d944a86dfca3d0a332f401b079db
chempkovsky <chempkovsky@rambler.ru>
2021-08-12 14:18:07 +0300

    php extension with symfony and doctrine

--- CS2WPF/Helpers/CustomPluralization.cs
+++ CS2WPF/Helpers/CustomPluralization.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 
 namespace CS2WPF.Helpers
 {
-    static class CustomPluralization
+    public static class CustomPluralization
     {
         public static PluralizationService pluralizationservice = PluralizationService.CreateService(new CultureInfo("en-us"));
 
@@ -11,21 +11,21 @@ namespace CS2WPF.Helpers
         {
             if (string.IsNullOrEmpty(str)) return "";
             if (pluralizationservice.IsPlural(str)) return str;
-            return pluralizationservice.Pluralize(str);
+            return CustomPluralization.pluralizationservice.Pluralize(str);
         }
         public static string Singularize(string str)
         {
             if (string.IsNullOrEmpty(str)) return "";
             if (pluralizationservice.IsSingular(str)) return str;
-            return pluralizationservice.Singularize(str);
+            return CustomPluralization.pluralizationservice.Singularize(str);
         }
         public static bool IsSingular(string str)
         {
-            return pluralizationservice.IsSingular(str);
+            return CustomPluralization.pluralizationservice.IsSingular(str);
         }
         public static bool IsPlural(string str)
         {
-            return pluralizationservice.IsPlural(str);
+            return CustomPluralization.pluralizationservice.IsPlural(str);
         }
     }
 }
