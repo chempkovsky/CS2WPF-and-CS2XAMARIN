2d10fcefc6d3ee2ad12b11753ca5e2ee7beff1f9
chempkovsky yury <chempkovsky@rambler.ru>
2022-08-18 22:13:58 +0300

    Datasource has been added to Xamarin project

--- CS2WPF/ViewModel/SelectFolderViewModel.cs
+++ CS2WPF/ViewModel/SelectFolderViewModel.cs
@@ -203,7 +203,8 @@ namespace CS2WPF.ViewModel
                     locFileName =
                         SelectedModel.ViewName + TrimPrefix(Path.GetFileNameWithoutExtension(T4SelectedFolder));
                 }
-                FileName = GetHyphenedName(locFileName);
+                //FileName = GetHyphenedName(locFileName);
+                FileName = locFileName;
             }
             locFileName = null;
             string[] files = Directory.GetFiles(Path.Combine(_T4RootFolder, T4SelectedFolder), "*.html");
